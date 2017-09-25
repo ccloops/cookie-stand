3c08419a588031e6d228caff9f0395be1b979f54
Catherine Looper <loopercat@gmail.com>
2017-09-25 15:42:02 -0700

    Added Objects for SeaTac, Seattle Center, Capitol Hill, and Alki

--- app.js
+++ app.js
@@ -26,15 +26,89 @@ var pike = {
 
 pike.render();
 
-// var seaTac = {
-//   storeName: 'SeaTac Airport',
-//   minCustomers: 3,
-//   maxCustomers: 24,
-//   avgSale: 1.2,
-//   render: function() {
-//
-//     for var i = 0; i < hours.length; i++) {
-//
-//     }
-//   }
-// }
+var seaTac = {
+  storeName: 'SeaTac Airport',
+  minCustomers: 3,
+  maxCustomers: 24,
+  avgSale: 1.2,
+  customerPerHour: function() {
+    return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers;
+  },
+
+  render: function() {
+
+    for (var i = 0; i < hours.length; i++) {
+      var elSeaTac = document.getElementById('seaTac');
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + ' : ' + this.customerPerHour() + ' cookies';
+      elSeaTac.appendChild(liEl);
+    }
+  }
+};
+
+seaTac.render();
+
+var center = {
+  storeName: 'Seattle Center',
+  minCustomers: 11,
+  maxCustomers: 38,
+  avgSale: 3.7,
+  customerPerHour: function() {
+    return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers;
+  },
+
+  render: function() {
+
+    for (var i = 0; i < hours.length; i++) {
+      var elCenter = document.getElementById('center');
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + ' : ' + this.customerPerHour() + ' cookies';
+      elCenter.appendChild(liEl);
+    }
+  }
+};
+
+center.render();
+
+var capitolHill = {
+  storeName: 'Capitol Hill',
+  minCustomers: 20,
+  maxCustomers: 38,
+  avgSale: 2.3,
+  customerPerHour: function() {
+    return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers;
+  },
+
+  render: function() {
+    for (var i = 0; i < hours.length; i++) {
+      var elCapitolHill = document.getElementById('capitolHill');
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + ' : ' + this.customerPerHour() + ' cookies';
+      elCapitolHill.appendChild(liEl);
+    }
+  }
+};
+
+capitolHill.render();
+
+
+var alki = {
+  storeName: 'Alki',
+  minCustomers: 2,
+  maxCustomers: 16,
+  avgSale: 4.6,
+  customerPerHour: function() {
+    return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers;
+  },
+
+  render: function() {
+    for (var i = 0; i < hours.length; i++) {
+      var elAlki = document.getElementById('alki');
+      var liEl = document.createElement('li');
+      liEl.textContent = hours [i] + ' : ' + this.customerPerHour() + ' cookies';
+      elAlki.appendChild(liEl);
+    }
+  }
+};
+
+alki.render();
