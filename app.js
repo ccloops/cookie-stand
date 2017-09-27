017ca7db58da8c5d654683c4b23ee6e499ecc7a1
Catherine Looper <loopercat@gmail.com>
2017-09-27 13:22:03 -0700

    Refactored code organization for cleaner look

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-
 var allStores = [];
-
 var storeTable = document.getElementById('store');
+var allStoreTotals = [];
+var totalTurtle = 0;
 
 function Store(storeName, minCustomers, maxCustomers, avgSale) {
   this.storeName = storeName;
@@ -54,13 +54,11 @@ Store.prototype.render = function() {
   storeTable.appendChild(trEl);
 };
 
-
 new Store('1st and Pike', 23, 65, 6.3);
 new Store('SeaTac Airport', 3, 24, 1.2);
 new Store('Seattle Center', 11, 38, 3.7);
 new Store('Capitol Hill', 20, 38, 2.3);
 new Store('Alki', 2, 16, 4.6);
-console.log(allStores);
 
 function makeHeaderRow() {
   var trEl = document.createElement('tr');
@@ -73,28 +71,25 @@ function makeHeaderRow() {
     thEl = document.createElement('th');
     thEl.textContent = hours[i];
     trEl.appendChild(thEl);
-
-    storeTable.appendChild(trEl);
   }
 
   thEl = document.createElement('th');
   thEl.textContent = 'Daily Totals';
   trEl.appendChild(thEl);
 
-}
-makeHeaderRow();
-
-for (var i in allStores) {
-  allStores[i].hourlyTransactions();
-  allStores[i].dailyTransactions();
-  allStores[i].render();
+  storeTable.appendChild(trEl);
 }
 
-var allStoreTotals = [];
-var totalTurtle = 0;
+function innerRows() {
+  for (var i in allStores) {
+    allStores[i].hourlyTransactions();
+    allStores[i].dailyTransactions();
+    allStores[i].render();
+  }
+}
 
 function columnSum() {
-  for(i = 0; i < hours.length; i++) {
+  for(var i = 0; i < hours.length; i++) {
     var storeTotal = 0;
     for(var j = 0; j < allStores.length; j++){
       storeTotal += allStores[j].hourlySales[i];
@@ -103,10 +98,10 @@ function columnSum() {
   }
 }
 
-columnSum();
-
-for (var k in allStoreTotals) {
-  totalTurtle += allStoreTotals[k];
+function totalTableSum() {
+  for (var k in allStoreTotals) {
+    totalTurtle += allStoreTotals[k];
+  }
 }
 
 function makeFooterRow() {
@@ -121,12 +116,21 @@ function makeFooterRow() {
     thEl.textContent = allStoreTotals[i];
     trEl.appendChild(thEl);
 
-    storeTable.appendChild(trEl);
   }
 
   thEl = document.createElement('th');
   thEl.textContent = totalTurtle;
   trEl.appendChild(thEl);
 
+  storeTable.appendChild(trEl);
 }
+
+makeHeaderRow();
+
+innerRows();
+
+columnSum();
+
+totalTableSum();
+
 makeFooterRow();
